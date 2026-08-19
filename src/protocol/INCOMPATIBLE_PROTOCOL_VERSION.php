d84edc8f0c87a786454eb8dcbb5ace5998006122
Ghost97400 <77570455+MyNameIs6758@users.noreply.github.com>
2026-08-19 09:21:00 +0400

    Initial commit RakLib 0.8.0

---
+++ src/protocol/INCOMPATIBLE_PROTOCOL_VERSION.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace raklib\protocol;
+
+use raklib\RakLib;
+
+class INCOMPATIBLE_PROTOCOL_VERSION extends Packet {
+
+    public static $ID = 0x1A;
+
+    public $protocolVersion;
+    public $serverID;
+
+    public function encode()
+    {
+        parent::encode();
+        $this->putByte($this->protocolVersion);
+        $this->put(RakLib::MAGIC);
+        $this->putLong($this->serverID);
+    }
+
+    public function decode(){
+        parent::decode();
+        $this->protocolVersion = $this->getByte();
+        $this->offset += 16; //MAGIC
+        $this->serverID = $this->getLong();
+    }
+}
